e4affe2797499fd350113c4fab6564c077ef5147
anandacallista11 <151838374+anandacallista11@users.noreply.github.com>
2024-12-12 14:05:04 +0700

    Update api.dart

--- lib/helpers/api.dart
+++ lib/helpers/api.dart
@@ -1,7 +1,7 @@
 class BaseUrl {
-  static String tambah = "http://192.168.100.40/biodata/create.php";
-  static String lihat = "http://192.168.100.40/biodata/details.php";
-  static String edit = "http://192.168.100.40/biodata/update.php";
-  static String hapus = "http://192.168.100.40/biodata/delete.php";
-  static String data = "http://192.168.100.40/biodata/list.php";
+  static String tambah = "http://192.168.100.48/projekbaru/create.php";
+  static String lihat = "http://192.168.100.48/projekbaru/details.php";
+  static String edit = "http://192.168.100.48/projekbaru/update.php";
+  static String hapus = "http://192.168.100.48/projekbaru/delete.php";
+  static String data = "http://192.168.100.48/projekbaru/list.php";
 }
